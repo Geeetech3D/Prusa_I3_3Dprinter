b1adf6f3b4b5a2a568ce52c1f4d4e0e840fad398
harlyh <harly@DESKTOP-GOTJ5BT.localdomain>
2018-10-12 14:16:22 +0100

    Fixed versoning. Cleanups

--- A10M_marlin1.1.9/Marlin/Version.h
+++ A10M_marlin1.1.9/Marlin/Version.h
@@ -48,7 +48,7 @@
    * here we define this default string as the date where the latest release
    * version was tagged.
    */
-  #define STRING_DISTRIBUTION_DATE "2018-10-12 12:00"
+  #define STRING_DISTRIBUTION_DATE "2018-10-12 14:00"
 
   /**
    * Required minimum Configuration.h and Configuration_adv.h file versions.
@@ -70,7 +70,7 @@
   /**
    * Defines a generic printer name to be output to the LCD after booting Marlin.
    */
-  #define MACHINE_NAME "3D Printer"
+  #define MACHINE_NAME "Geeetech A10M"
 
   /**
    * The SOURCE_CODE_URL is the location where users will find the Marlin Source
