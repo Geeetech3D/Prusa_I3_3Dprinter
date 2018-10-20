b9d233fc470f8314f0e3c8dc3fb96602cadae358
Scott Lahteine <github@thinkyhead.com>
2018-10-20 00:45:32 -0500

    Fixups for configuration/boards

--- A10M_marlin1.1.8/Configuration and hex file/no 3d touch/Configuration.h
+++ A10M_marlin1.1.8/Configuration and hex file/no 3d touch/Configuration.h
@@ -797,8 +797,8 @@
 #define Y_BED_SIZE 220
 
 // Travel limits (mm) after homing, corresponding to endstop positions.
-#define X_MIN_POS 0
-#define Y_MIN_POS 0
+#define X_MIN_POS -10
+#define Y_MIN_POS -5
 #define Z_MIN_POS 0
 #define X_MAX_POS X_BED_SIZE
 #define Y_MAX_POS Y_BED_SIZE
--- A10M_marlin1.1.8/Marlin/Configuration.h
+++ A10M_marlin1.1.8/Marlin/Configuration.h
@@ -734,7 +734,7 @@
  *     But: `M851 Z+1` with a CLEARANCE of 2  =>  2mm from bed to nozzle.
  */
 #define Z_CLEARANCE_DEPLOY_PROBE   10 // Z Clearance for Deploy/Stow
-#define Z_CLEARANCE_BETWEEN_PROBES 0// 5 // Z Clearance between probe points.......
+#define Z_CLEARANCE_BETWEEN_PROBES  5 // Z Clearance between probe points
 
 // For M851 give a range for adjusting the Z probe offset
 #define Z_PROBE_OFFSET_RANGE_MIN -20
@@ -802,8 +802,8 @@
 #define Y_BED_SIZE 220
 
 // Travel limits (mm) after homing, corresponding to endstop positions.
-#define X_MIN_POS 0
-#define Y_MIN_POS 0
+#define X_MIN_POS -10
+#define Y_MIN_POS -5
 #define Z_MIN_POS 0
 #define X_MAX_POS X_BED_SIZE
 #define Y_MAX_POS Y_BED_SIZE
